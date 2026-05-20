f95205963dcf428baa2ecb423cf8a7530d41c5cc
Ferdaus Alamsyah <ferdausalamsyah@gmail.com>
2026-05-20 13:25:10 +0800

    1.0.6

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const APP_VERSION = '1.0.5';
+const APP_VERSION = '1.0.6';
 const API_URL = 'https://script.google.com/a/macros/bakkah.sch.id/s/AKfycbzIek6qAuyDTapYx4IzmVxDqYJdeF0wxUB1pQuOuqlsETUYnv2ZOe0GrTn0Bt1mKCkZ4A/exec';
 
 // ── API Cache ──
